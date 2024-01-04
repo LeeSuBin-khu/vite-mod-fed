5155b159cd012c1a9610f7c9970b51decacce1a2
Subin Lee <subinlee@nineright.com>
2024-01-04 12:54:18 +0900

    remote

--- remote/src/main.jsx
+++ remote/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+
 import App from './App.jsx'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import AuthProvider from '../../host/src/contexts/AuthProvider/index.jsx'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RecoilRoot>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </RecoilRoot>
 )
--- remote/vite.config.js
+++ remote/vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-// https://vitejs.dev/config/
+// remote
 export default defineConfig({
   plugins: [
     react(),
     federation({
       name: "remote-app",
       filename: "remoteEntry.js",
-      // Modules to expose
+      remotes: {
+        remoteApp: "http://localhost:5000/dist/assets/remoteEntry.js",
+      },
       exposes: {
         "./Button": "./src/components/Button",
         "./store/countStore": "./src/store/countStore",
